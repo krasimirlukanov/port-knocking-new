7289a63a8f0f335a1c2c9a202fb532e92eff7be5
Krasimir Lukanov <k@lukanov.bg>
2023-07-17 17:43:32 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from tools.JsonManager import JsonManager
+from tools.KnockManager import KnockManager
+from tools.ProfileManager import ProfileManager
+from tools.IPManager import IPManager
+
+
+json_man = JsonManager()
+knock_man = KnockManager()
+profile_man = ProfileManager()
+ip_man = IPManager()
